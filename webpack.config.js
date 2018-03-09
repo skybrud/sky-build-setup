c2282aab8a478bc1b79bc991cb34d40b1ca81ab9
Rudi Ørnhøj <rornhoj@gmail.com>
2018-03-09 14:54:54 +0100

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -2,90 +2,86 @@ const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const merge = require('webpack-merge');
 const path = require('path');
-const packageJson = require('./package.json');
 
-const name = (() => {
-	const capitalise = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-	const nameArray = packageJson.name.split('-');
+function init(projectName, requestedBuild) {
+	const name = (() => {
+		const capitalise = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+		const nameArray = projectName.split('-');
 
-	return `${capitalise(nameArray[0])}${capitalise(nameArray[1])}`;
-})();
+		return `${capitalise(nameArray[0])}${capitalise(nameArray[1])}`;
+	})();
 
-const baseConfig = {
-	output: {
-		path: path.resolve(__dirname + '/dist/'),
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				include: __dirname,
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					preserveWhitespace: false,
-					postcss: [
-						require('autoprefixer')(),
-					],
-					loaders: {
-						css: 'vue-style-loader!css-loader!sass-loader',
+	const baseConfig = {
+		output: {
+			path: path.resolve(__dirname + '/dist/'),
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					include: __dirname,
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
+					options: {
+						preserveWhitespace: false,
+						postcss: [
+							require('autoprefixer')(),
+						],
+						loaders: {
+							css: 'vue-style-loader!css-loader!sass-loader',
+						},
 					},
 				},
-			},
-		],
-	},
-	plugins: [
-		new UglifyJsPlugin({
-			sourceMap: false,
-			uglifyOptions: {
-				mangle: true,
-				compress: {
-					warnings: false,
+			],
+		},
+		plugins: [
+			new UglifyJsPlugin({
+				sourceMap: false,
+				uglifyOptions: {
+					mangle: true,
+					compress: {
+						warnings: false,
+					},
 				},
+			}),
+		],
+	};
+
+	const builds = {
+		module: {
+			target: 'node',
+			entry: path.resolve(__dirname + '/src/' + name + '.vue'),
+			output: {
+				filename: name.toLowerCase() + '.js',
+				libraryTarget: 'umd',
+				library: name,
+				umdNamedDefine: true,
 			},
-		}),
-	],
-};
+		},
+		plugin: {
+			entry: path.resolve(__dirname + '/src/plugin.js'),
+			output: {
+				filename: name.toLowerCase() + '.min.js',
+				libraryTarget: 'window',
+				library: name,
+			}
+		},
+		service: {
+			entry: path.resolve(__dirname + '/src/' + name + '.js'),
+			output: {
+				filename: name.toLocaleLowerCase() + '.js',
+				libraryTarget: 'umd',
+				library: name,
+				umdNamedDefine: true,
+			},
+		}
+	};
 
-const moduleConfig = {
-	target: 'node',
-	entry: path.resolve(__dirname + '/src/' + name + '.vue'),
-	output: {
-		filename: name.toLowerCase() + '.js',
-		libraryTarget: 'umd',
-		library: name,
-		umdNamedDefine: true,
-	},
+	return merge(baseConfig, builds[requestedBuild]);
 }
 
-const pluginConfig = {
-	entry: path.resolve(__dirname + '/src/plugin.js'),
-	output: {
-		filename: name.toLowerCase() + '.min.js',
-		libraryTarget: 'window',
-		library: name,
-	}
-}
-
-const serviceConfig = {
-	entry: path.resolve(__dirname + '/src/' + name + '.js'),
-	output: {
-		filename: name.toLocaleLowerCase() + '.js',
-		libraryTarget: 'umd',
-		library: name,
-		umdNamedDefine: true,
-	},
-}
-
-const config = name === 'SkyWindow'
-	? merge(baseConfig, serviceConfig)
-	: [
-		merge(baseConfig, pluginConfig),
-		merge(baseConfig, moduleConfig),
-	];
-
-module.exports = config;
+module.export = init;
