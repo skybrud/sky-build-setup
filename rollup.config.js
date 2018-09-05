324959b738a1c9543dec8dd1d2be02258be357ce
Rudi Ørnhøj <rornhoj@gmail.com>
2018-09-05 09:29:42 +0200

    Rollup 3.0.0 no SSR

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,21 @@
-// rollup.config.js
 const vue = require('rollup-plugin-vue');
 const buble = require('rollup-plugin-buble');
-const uglify = require('rollup-plugin-uglify-es');
-const minimist = require('minimist');
 const path = require('path');
 
-const argv = minimist(process.argv.slice(2));
+function init(packageJson, buildRoot) {
+	const pascaledName = (() => {
+		const pascalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+		const nameArray = packageJson.name.split('-');
 
-const config = {
-    input: 'src/index.js',
-    output: {
-        name: '{{componentNamePascal}}',
-        exports: 'named',
-    },
-    plugins: [
-        vue({
-            css: true,
-            compileTemplate: true,
-        }),
-        buble(),
-    ],
-};
-
-// Only minify browser (iife) version
-if (argv.format === 'iife') {
-    config.plugins.push(uglify());
-}
-
-// export default config;
-
-function init(projectName, requestedBuild, buildRoot) {
-	const name = (() => {
-		const capitalise = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-		const nameArray = projectName.split('-');
-
-		return nameArray.reduce((acc, cur) => { return acc += capitalise(cur)}, '');
+		return nameArray.reduce((acc, cur) => { return acc += pascalize(cur)}, '');
 	})();
 
-	const argv = minimist(process.argv.slice(2));
-
 	const config = {
-		input: path.resolve(buildRoot + 'src/index.js'),
+		external: Object.keys(packageJson.dependencies),
+		input: path.resolve(buildRoot + '/src/index.js'),
 		output: {
-			name,
-			dir: path.resolve(buildRoot + 'dist/'),
+			name: pascaledName,
+			dir: path.resolve(buildRoot + '/dist/'),
 			exports: 'named',
 		},
 		plugins: [
@@ -54,11 +26,6 @@ function init(projectName, requestedBuild, buildRoot) {
 			buble(),
 		],
 	};
-
-	// Only minify browser (iife) version
-	if (argv.format === 'iife') {
-		config.plugins.push(uglify());
-	}
 
 	return config;
 };
